0589fdcb5c890eec42694790bb37b0618443b839
bnf <umukorocaleb0@gmail.com>
2026-03-10 20:47:48 +0100

    feat: use premium non-human profile icon for colleagues without photos

--- components/FamiliesTestimonies.tsx
+++ components/FamiliesTestimonies.tsx
@@ -16,21 +16,21 @@ export default function Testimonials() {
       text: "I would like to acknowledge my colleague Jide for his consistent professionalism, teamwork, and dedication. He always communicate clearly, support the team when needed and is an outstanding nurse. His reliability and positive attitude makes a meaningful contribution to work environment. I have always enjoyed working with him.",
       name: "Agnes Stansfeld",
       position: "RN, ITU Nurse, London, UK",
-      image: "/premium_avatar_agnes.png",
+      image: "/premium_profile_icon.png",
     },
     {
       id: 3,
       text: "I had the privilege of working with Olutoyin when she served as a deputy manager in a domiciliary care service in Leeds, working closely alongside the registered manager. She consistently demonstrated exceptional leadership and a deep commitment to high-quality, person-centred care. I witnessed her bring tender, thoughtful support to our care-at-home services, always striving to ensure that people receiving care felt safe, valued, and happy in the comfort of their own homes. She is hands-on, compassionate, and leads by example, whether supporting clients and their families or managing the wider team. Her humility, dedication, and genuine heart for service make her a standout leader and an asset to any organisation.",
       name: "Christy A.",
       position: "Colleague",
-      image: "/premium_avatar_christy.png",
+      image: "/premium_profile_icon.png",
     },
     {
       id: 4,
       text: "All through the time I worked with Olutoyin as a deputy manager in residential support care, she proved to be an outstanding leader and support worker. She brought a level of tenderness, empathy, and professionalism that deeply impacted both residents and staff. Her leadership style is grounded in compassion and active involvement, always prioritising the needs of those in her care. Olutoyin’s dedication, humility, and ability to inspire others truly set her apart.",
       name: "Leslie S.",
       position: "Colleague",
-      image: "/premium_avatar_leslie.png",
+      image: "/premium_profile_icon.png",
     },
   ];
 
